bdc732226d4944424c655a5275984f1520c08646
lmsv-mx123 <salazar.luis.miguel@gmail.com>
2016-09-17 15:02:51 -0500

    Fix in checking empty elements towards count

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -15,7 +15,7 @@
           $scope.message = "Please enter data first!";
           $scope.result = "error";
         } else{
-          var list_arr = list.split(",");
+          var list_arr = list.split(",").map((item) => item.trim());
           if(list_arr.filter(String).length <=3){
             $scope.message = "Enjoy!";
             $scope.result = "success";

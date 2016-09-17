f1813a66a0dafaa35dbe2d8e1c0ad23f9f87892a
lmsv-mx123 <salazar.luis.miguel@gmail.com>
2016-09-17 14:55:48 -0500

    Adding module1 solution

---
+++ module1-solution/js/app.js
@@ -0,0 +1,31 @@
+(function(){
+  'use strict';
+
+  angular.module('LunchChecker', [])
+  .controller('LunchCheckerController', function($scope){
+
+    $scope.input_list = "";
+    $scope.message = "";
+    $scope.result = "";
+
+    $scope.Calculate = function(){
+        var list = $scope.input_list;
+        // ng-model already performed the trimming
+		if(list == ""){
+          $scope.message = "Please enter data first!";
+          $scope.result = "error";
+        } else{
+          var list_arr = list.split(",");
+          if(list_arr.filter(String).length <=3){
+            $scope.message = "Enjoy!";
+            $scope.result = "success";
+          }else{
+            $scope.message = "Too Much!";
+            $scope.result = "success";
+          }
+        }
+    };
+
+  });
+
+})();
